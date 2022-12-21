7b49b95f497ea16bc72b324f05616850c411a090
Rodolphe Fontaine <30529563+Rodi26@users.noreply.github.com>
2022-12-21 11:06:31 +0100

    add provider random 3.4.3 and pgp key

---
+++ env_random3.4.3_darwin_arm64.sh
@@ -0,0 +1,9 @@
+export OS=darwin
+export ARCH=arm64
+export PROVIDER_NAME=random
+export PROVIDER_VERSION=3.4.3
+export SHASUMS_FILE=providers/random/3.4.3/terraform-provider-random_3.4.3_SHA256SUMS
+export SHASUMS_SIG_FILE=providers/random/3.4.3/terraform-provider-random_3.4.3_SHA256SUMS.sig
+export ZIP_FILE=providers/random/3.4.3/terraform-provider-random_3.4.3_darwin_arm64.zip
+export FILENAME=terraform-provider-random_3.4.3_darwin_arm64.zip
+
---
+++ env_random3.4.3_windows_amd64.sh
@@ -0,0 +1,9 @@
+export OS=windows
+export ARCH=amd64
+export PROVIDER_NAME=random
+export PROVIDER_VERSION=3.4.3
+export SHASUMS_FILE=providers/random/3.4.3/terraform-provider-random_3.4.3_SHA256SUMS
+export SHASUMS_SIG_FILE=providers/random/3.4.3/terraform-provider-random_3.4.3_SHA256SUMS.sig
+export ZIP_FILE=providers/random/3.4.3/terraform-provider-random_3.4.3_windows_amd64.zip
+export FILENAME=terraform-provider-random_3.4.3_windows_amd64.zip
+
